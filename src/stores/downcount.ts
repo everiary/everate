7141bea8517e25f656b56c16661374bf2f131261
wemsx <3551697397@qq.com>
2023-10-28 21:54:42 +0800

    chore: 加入pinia插件并初始化pinia

---
+++ src/stores/downcount.ts
@@ -0,0 +1,38 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+interface Downcount {
+  title: string,
+  date: {
+    y: number,// 0代表每年/月对应日期
+    m: number,
+    d: number
+  }
+}
+
+export const useDownCountStore = defineStore(
+  "downcount",
+  () => {
+    const downcountEG: Downcount = {
+      title: '新年',
+      date: {
+        y: 2024,
+        m: 1,
+        d: 1
+      }
+    }
+    const data = ref([downcountEG,{
+      title: '喜欢Ta 2年整',
+      date: {
+        y: 2024,
+        m: 5,
+        d: 20
+      }
+    }])
+
+    return { data };
+  },
+  {
+    persist: true,
+  }
+);
