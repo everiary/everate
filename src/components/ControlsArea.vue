70a3b3ba7b06245cba6712975c8d831601ffc196
wemsx <3551697397@qq.com>
2023-10-29 10:59:42 +0800

    feat: 改进了控制区域的功能，预计下个版本进行完善。

---
+++ src/components/ControlsArea.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="controlBars">
+        <button @click="a">Settings</button>
+        <button @click="b">About</button>
+    </div>
+</template>
+<script setup>
+const a = () => { alert('1') }
+const b = () => { alert('2') }
+</script>
+<style scoped>
+.controlBars {
+    position: absolute;
+    left: 10%;
+    width: 80%;
+    display: flex;
+    grid-column-gap: 2vw;
+}
+
+button {
+    background-color: rgba(255, 255, 255, 0.1);
+    padding: 0.5vw 1.2vw;
+    border: 0.1vw solid rgba(84, 84, 84, .65);
+    border-radius: 0.4vw;
+    font-size: 1vw;
+    font-weight: 600;
+}
+</style>
--- src/components/OperateArea.vue
+++ src/components/OperateArea.vue
@@ -2,32 +2,36 @@
     <div class="operate">
         <div class="visible">
             <div class="avatar">111</div>
-            <div class="whito">111</div>
+            <div class="whito"><HitokotoBlock /></div>
             <div class="downcount"><DowncountArea /></div>
+            <div class="controls"><ControlsArea /></div>
         </div>
     </div>
 </template>
 
 <script setup>
 import DowncountArea from "./DowncountArea.vue";
+import HitokotoBlock from "./HitokotoBlock.vue";
+import ControlsArea from "./ControlsArea.vue";
 </script>
 
 <style scoped>
 .operate {
+    position: absolute;
+    left: 60vw;
     background: #171717;
     background: rgba(23, 23, 23, 0.9);
-    width: 100vw;
-    margin-left: 50vw;
+    width: 40vw;
     height: 100vh;
     border-radius: 75px 0px 0px 75px;
     -moz-border-radius: 75px 0px 0px 75px;
     -webkit-border-radius: 75px 0px 0px 75px;
-    animation-duration: 1s;
+    animation-duration: 1.5s;
     animation-fill-mode: both;
     animation-name: fadeInLeft;
 }
 .visible{
-    width: 50vw;
+    width: 40vw;
     height: 100vh;
     display: flex;
     flex-direction: column;
@@ -38,12 +42,30 @@ import DowncountArea from "./DowncountArea.vue";
 }
 
 .whito {
+    position: relative;
     width: 100%;
     height: 30vh;
 }
 
 .downcount {
+    position: relative;
     width: 100%;
-    height: 40vh;
+    height: 30vh;
+}
+.controls {
+    position: relative;
+    width: 100%;
+    height: 10vh;
+}
+@keyframes fadeInLeft {
+  from {
+    opacity: 0;
+    transform: translate3d(100%, 0, 0);
+  }
+
+  to {
+    opacity: 1;
+    transform: none;
+  }
 }
 </style>
