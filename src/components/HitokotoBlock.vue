544e0dcf8594bcc99d2d2ae756c7c9695316c176
wemsx <3551697397@qq.com>
2023-10-29 10:58:47 +0800

    feat: 新增hitokoto功能。

---
+++ src/components/HitokotoBlock.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="hitokoto" @click="getHito">{{ hito }}</div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const hito = ref('')
+
+const getHito = () => {
+    fetch('https://v1.hitokoto.cn')
+        .then(response => response.json())
+        .then(data => {
+            hito.value = data.hitokoto
+        })
+        .catch(console.error)
+}
+getHito()
+
+</script>
+<style scoped>
+#hitokoto {
+    position: absolute;
+    left: 10%;
+    top: 50%;
+    width: 80%;
+    text-align: center;
+    font-size: 2vw;
+    color: #f9f4dc;
+    font-family: "Microsoft Yahei";
+    overflow: hidden;
+}
+</style>
