e74c49579402f4e0dc5ca5645f86081ea7974dbf
wemsx <3551697397@qq.com>
2023-11-04 18:15:44 +0800

    feat: 支持使用动态图片作为背景

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
 import CalendarArea from './components/CalendarArea.vue';
 import OperateArea from './components/OperateArea.vue';
+const imageUrl = 'https://source.unsplash.com/collection/94734566/1920x1080'
 </script>
 
 <template>
-    <CalendarArea />
-    <OperateArea />
+    <div id="wallpaper" :style="{backgroundImage: `url(${imageUrl})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}">
+        <CalendarArea />
+        <OperateArea />
+    </div>
 </template>
 
 <style scoped>
+#wallpaper {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    overflow: hidden;
+}
 </style>
 
 <style>
 #app {
-    background-image: url("/e.jpg");
-    background-repeat: no-repeat;
-    background-size: cover;
     position: absolute;
     left: 0px;
     top: 0px;
