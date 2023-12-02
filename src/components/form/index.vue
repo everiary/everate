1c273bba415935b30f6c8d442e08c32639ae26bd
wemsx <3551697397@qq.com>
2023-12-02 15:11:54 +0800

    style: 修改模态框样式，修改关闭逻辑。

--- src/components/form/index.vue
+++ src/components/form/index.vue
@@ -1,13 +1,15 @@
 <template>
     <Teleport to="#app">
-        <div class="formClass fixed left-0 top-0 w-100vw h-100vh flexCenter mask" v-show="visible" >
-            <div class="formFramework fixed w-40vw h-60vh rd-0.8vw flex flex-col justify-center">
-                <div class="formHeader items-end h-10vh">
+        <div class="formMain fixed left-0 top-0 w-100vw h-100vh flexCenter " v-show="visible">
+            <div class="formClass fixed left-0 top-0 w-100vw h-100vh flexCenter mask -z-1"
+                @click="$emit('update:visible', false)"></div>
+            <div class="formFramework">
+                <!--<div class="formHeader items-end h-10vh">
                     <span id="formTitle" class="absolute p-1vw left-0 font-size-3vw Yahei">{{ title }}</span>
                     <span id="crossBtn" class="absolute p-1vw right-0"><button @click="$emit('update:visible', false)" style="padding: 0.5vw;font-size: 1vw;">x</button></span>
-                </div>
-                <div class="formBody h-40vh">
-                    <slot></slot>
+                </div>-->
+                <div class="formBody">
+                    <slot />
                 </div>
                 <div class="formFooter flex items-center justify-end h-10vh">
                     <button style="margin-right: 2vw;" @click="$emit('update:visible', false)">cancel</button>
@@ -28,8 +30,35 @@ const title = ref('wemsx')
 </script>
 
 <style scoped>
-
 .formFramework {
-    background: rgba(250,250,250,0.8);
+    width: 400px;
+    background: linear-gradient(#212121, #212121) padding-box,
+        linear-gradient(145deg, transparent 35%, #e81cff, #40c9ff) border-box;
+    border: 2px solid transparent;
+    padding: 32px 24px;
+    font-size: 14px;
+    font-family: inherit;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    box-sizing: border-box;
+    border-radius: 16px;
+    background-size: 200% 100%;
+    animation: gradient 5s ease infinite;
+}
+
+@keyframes gradient {
+    0% {
+        background-position: 0% 50%;
+    }
+
+    50% {
+        background-position: 100% 50%;
+    }
+
+    100% {
+        background-position: 0% 50%;
+    }
 }
 </style>
