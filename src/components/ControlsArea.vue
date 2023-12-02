09eb7990092775510f0ad16446c77f0e5b3f8fd1
wemsx <3551697397@qq.com>
2023-12-02 18:02:41 +0800

    feat: 新增表单交互功能。

--- src/components/ControlsArea.vue
+++ src/components/ControlsArea.vue
@@ -1,24 +1,36 @@
 <template>
     <div class="controlBars absolute flexCenter left-12% w-80% grid-gap-3vw">
         <button @click="settingVisible = !settingVisible" class="settings">Settings<span class="arrow"> ›</span></button>
-        <formDev v-model:visible="settingVisible">
-            <p>1433223</p>
-        </formDev>
+        <formBlock v-model:visible="settingVisible">
+            <div class="overflow-hidden">
+            <ul class="w-full p-0">
+                <li v-for="item of data" class="list-none">{{ item.title + ' : ' + item.is }}
+                    <span class="relative float-right">
+                        <input class="checkboxInput" :id="item.title" type="checkbox" v-model="item.is" /><label class="toggleSwitch" :for="item.title"></label>
+                    </span>
+                </li>
+            </ul>
+            </div>
+        </formBlock>
         <button @click="aboutVisible = !aboutVisible">About<span class="arrow"> ›</span></button>
-        <formDev v-model:visible="aboutVisible">
+        <formBlock v-model:visible="aboutVisible">
             <p>1</p>
-        </formDev>
+        </formBlock>
 
     </div>
 </template>
 
 <script setup lang="ts">
-import formDev from './form/index.vue'
+import formBlock from './form/index.vue'
 import { ref } from 'vue'
 import type { Ref } from 'vue'
 
 let settingVisible: Ref<boolean> = ref(false)
 let aboutVisible: Ref<boolean> = ref(false)
+let data = ref([
+    { title: '用户名', value: 'wemsx', is: false },
+    { title: '头像地址', value: 'https://cravatar.cn/avatar/95d35c34b1302443c6b94a2aa74a2065', is: false }
+])
 </script>
 
 <style scoped>
@@ -51,4 +63,47 @@ button:active {
         var(--accent) 0px 0px 30px 5px;
     background-color: var(--accent);
     transform: scale(0.95);
-}</style>
+}
+
+/* To hide the checkbox */
+.checkboxInput {
+    display: none;
+}
+
+.toggleSwitch {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    width: 50px;
+    height: 17px;
+    background-color: rgb(82, 82, 82);
+    border-radius: 20px;
+    cursor: pointer;
+    transition-duration: .2s;
+}
+
+.toggleSwitch::after {
+    content: "";
+    position: absolute;
+    height: 22px;
+    width: 22px;
+    left: 0px;
+    background-color: rgb(41, 41, 41);
+    border-radius: 50%;
+    transition-duration: .2s;
+    border: 2px solid rgb(82, 82, 82);
+}
+
+.checkboxInput:checked+.toggleSwitch::after {
+    transform: translateX(100%);
+    transition-duration: .2s;
+    border: 2px solid rgb(165, 255, 105);
+}
+
+/* Switch background change */
+.checkboxInput:checked+.toggleSwitch {
+    background-color: rgb(165, 255, 105);
+    transition-duration: .2s;
+}
+</style>
