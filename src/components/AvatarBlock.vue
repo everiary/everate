a240ad0af9e14605c4b92315fcba53927398b647
wemsx <3551697397@qq.com>
2023-12-03 09:41:10 +0800

    style: 放弃了头像显示方案，该用项目图标。

--- src/components/AvatarBlock.vue
+++ src/components/AvatarBlock.vue
@@ -1,8 +1,7 @@
 <template>
     <div id="avatar">
-        <img src="https://cravatar.cn/avatar/95d35c34b1302443c6b94a2aa74a2065" class="w-12vw mt-20vh"/>
+        <img src="/icon.svg" class="w-12vw mt-15vh"/>
     </div>
 </template>
 <script setup lang="ts">
-
 </script>
