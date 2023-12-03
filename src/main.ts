94798c83352ac9a47c05c578711ef2bb9ddb2cd4
wemsx <3551697397@qq.com>
2023-12-03 09:39:25 +0800

    fix: 修复pinia注入失败

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import pinia from "./stores/index";
 import App from './App.vue'
 import 'uno.css'
 
 const app = createApp(App)
-
-app.use(createPinia())
+app.use(pinia);
 
 app.mount('#app')
