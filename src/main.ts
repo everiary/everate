04bf79a1bcc77e4eac5fdf6c6d99ae040b599e22
wemsx <3551697397@qq.com>
2023-12-16 15:53:33 +0800

    feat: 简化 Canvas 背景引入流程

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,18 @@ import { createApp } from 'vue'
 import pinia from "./stores/index";
 import App from './App.vue'
 import 'uno.css'
-import { useRegisterSW } from 'virtual:pwa-register/vue';
+import { drawStars } from './scripts/star.esm';
+/*import { useRegisterSW } from 'virtual:pwa-register/vue';
 
 useRegisterSW({
     // 每小时检查一次
     onRegistered: r => r && setInterval(async () => await r.update(), 3600000),
     // 注册失败则报错到 console
     onRegisterError: error => console.error(error)
-});
+});*/
 
 const app = createApp(App)
 app.use(pinia);
-
 app.mount('#app')
+
+drawStars()
