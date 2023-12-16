161d83a1ff542e98297e7e5862f968492bbcf02e
wemsx <3551697397@qq.com>
2023-12-16 21:41:39 +0800

    chore: 常规修改.

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,20 @@
-import UnoCSS from 'unocss/vite'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-//import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath, URL } from 'node:url';
+import UnoCSS from 'unocss/vite';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   plugins: [
     UnoCSS({
       configFile: './uno.config.ts'
     }),
     vue(),
-    /*VitePWA({
-      manifest: {
-        name: "everate",
-        description: "A lightened browser start page.",
-        theme_color: "#171717",
-        icons: [{
-          src: "/icon.svg",
-          sizes: "any",
-          type: "image/svg+xml"
-        }]
-      },
-      registerType: 'autoUpdate',
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /(.*?)\.(js|css|ts)/, // js /css /ts静态资源缓存
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'js-css-cache',
-            },
-          },
-        ],
-      },
-      includeAssets: ['icon.svg'],
-      devOptions: {
-        enabled: true,
-        type: "module"
-    }
-    }),*/
   ],
   build: {
     minify: 'terser',
