183db62b536fc9d92e5900f07249df66b33a2f90
wemsx <3551697397@qq.com>
2023-12-17 10:16:18 +0800

    build: 构建xCr44ljb

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,27 @@
-import { fileURLToPath, URL } from 'node:url';
-import UnoCSS from 'unocss/vite';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from "node:url";
+import UnoCSS from "unocss/vite";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   plugins: [
     UnoCSS({
-      configFile: './uno.config.ts'
+      configFile: "./uno.config.ts",
     }),
     vue(),
   ],
   build: {
-    minify: 'terser',
+    minify: "terser",
     terserOptions: {
       compress: {
         drop_console: true,
-        drop_debugger: true
-      }
-    }
-  }
-})
+        drop_debugger: true,
+      },
+    },
+  },
+});
